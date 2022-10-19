854fd1efdc5b0305bbe3761819e336bf10b37dce
angelique KRAUSE <krause_angelique@hotmail.com>
2022-10-19 13:28:44 +0200

    COMMENTAIRES

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Ajout DYNAMIQUE d'une liste 
 
-const addItems = document.querySelector(".add-items");
+const addItems = document.querySelector(".add-items");//(1)ajout d'une variable pour le form
 const plateList = document.querySelector(".plates");
 // 1. const items = []; au départ items est vide
 // const items = addItems[0].value;
 const items = JSON.parse(localStorage.getItem("items")) || []; // on a pris la valeur et on a tapé les items dedans
 
+// écriture de l'objet en JSON
+const cat = {
+    'name' : 'Lucky'
+}
 
 console.log(addItems); //
 console.log(addItems[0].value); 
@@ -20,11 +24,12 @@ console.log(addItems[0].value);
 function handleAddItem(e) {
     e.preventDefault(); // empêche les reload intempestif
     //const item = document.querySelector(".name")// la valeur de l'item c'est son TEXTE
-    const item = addItems[0].value;
+    const item = addItems[0].value; // (2) Vérifier si on récupère la bonne valeur
     console.log();
-    items.push(item); // les items qui ont été enregistrés par l'utilisateur
+    items.push(item); // (3)les items qui ont été enregistrés par l'utilisateur dans l'array
     addItemList(items, plateList);
     // les clefs "items"
+    // (6) le localStorage retourne 0 : premier item écrit par l'utilisateur
     localStorage.setItem("items", JSON.stringify(items));
     console.log(item);
     
@@ -33,16 +38,17 @@ function handleAddItem(e) {
 //injection js
 // param est tapas 
 // function addItemList(param) {
+    // obliger que le premier paramètre soit un tableau
     function addItemList(items = [], itemList) {
 
     // return `<li>${param}</li>`;
     // plateList.innerHTML = `<li>${param}</li>`; // l'objectif est de pouvoir écrire le code dans la liste au dessus des input
     //itemList.innerHTML = items.map(element, index);
 itemList.innerHTML = items
-.map((item, index) => { // les items renvoyés le sont dans un tableau donc séparés par une virgule pour les séparer de façon différente on passera par join
-    return `<li>${index + 1}. ${item}</li>`; // pour renvoyer les infos  
+.map((item, index) => { // (4) les items renvoyés le sont dans un tableau donc séparés par une virgule pour les séparer de façon différente on passera par join
+    return `<li>${index + 1}. ${item}</li>`; // (5) pour renvoyer les infos  et +1 pour avoir le chiffre avant l'item 
 })
-.join("");
+.join(""); // jointure entre les différents 'objets' item 
 
 
 }
